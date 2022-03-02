b3c8eac4b25887c66316ecfc4935daddb371db37
Erl Jocson <jocsonerl@gmail.com>
2022-03-02 23:36:32 +0800

    added html files and styles

--- ReminderDjangoProject/ReminderDjangoProject/urls.py
+++ ReminderDjangoProject/ReminderDjangoProject/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('/', include('reminders.urls')),
+    path('', include('reminders.urls')),
     path('admin/', admin.site.urls),
 ]
--- ReminderDjangoProject/reminders/urls.py
+++ ReminderDjangoProject/reminders/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('home/', index_view, name='index'),
+    path('', index_view, name='index'),
     path('add-reminder/', add_reminder_view, name='add-reminder')
 ]
