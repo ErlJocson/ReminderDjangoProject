01604174b0bd747a552063422db0eb7f9384647d
Erl Jocson <jocsonerl@gmail.com>
2022-03-03 11:19:17 +0800

    added auth app

--- ReminderDjangoProject/ReminderDjangoProject/urls.py
+++ ReminderDjangoProject/ReminderDjangoProject/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('reminders.urls')),
+    path('auth/', include('auth.urls')),
     path('admin/', admin.site.urls),
 ]
--- ReminderDjangoProject/reminders/views.py
+++ ReminderDjangoProject/reminders/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from .forms import ReminderForm
+from .models import Reminders
 
 def index_view(request):
-    return render(request, 'index.html', {})
+    reminders = Reminders.objects.all()
+    return render(request, 'index.html', {
+        "reminders":reminders
+    })
 
 def add_reminder_view(request):
     form = ReminderForm(request.POST or None)
+    if form.is_valid():
+        form.save()
     return render(request, 'add_reminder.html', {
         "form":form
     })
