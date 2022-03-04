641a6943ff3b53c75a3799be50c6db57b0469384
Erl Jocson <jocsonerl@gmail.com>
2022-03-04 21:05:32 +0800

    add delete button

--- ReminderDjangoProject/reminders/urls.py
+++ ReminderDjangoProject/reminders/urls.py
@@ -3,5 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('', index_view, name='index'),
-    path('add-reminder/', add_reminder_view, name='add-reminder')
+    path('add-reminder/', add_reminder_view, name='add-reminder'),
+    path('delete-reminder/<int:id>', delete_reminder, name="delete-reminder")
 ]
